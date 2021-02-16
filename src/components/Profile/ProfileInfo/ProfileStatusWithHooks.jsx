d5e74c460572192ad7edb9bd19419e2926e9a581
Dimon170920 <dimon17092011@gmail.com>
2021-02-16 21:57:49 +0200

    page deployed

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const  ProfileStatusWithHooks = (props) => {
+
+    return (
+            <div>
+                {
+                <div>
+                    <span>{props.status||"-------"} </span>
+                </div>
+                }
+                {false &&
+                <div>
+                    <input   autoFocus={true} />
+                </div>
+                }
+            </div>
+        )
+
+}
+
+export default ProfileStatusWithHooks;
